fb1a46dbc625a597757f39e986c16a11977db8e1
Luiz Felipe Matias Xavier <luizfmxavier@gmail.com>
2024-09-18 15:25:30 -0300

    criação do adapter

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,85 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
+
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
     }
 }
+
+val contacts = listOf(
+    Contact(
+         "Maria",
+        "(55) 11 9854-9857",
+        R.drawable.sample1
+    ),
+    Contact(
+        "Pedro",
+        "(55) 11 9854-8975",
+        R.drawable.sample2
+    ),
+    Contact(
+        "José",
+        "(55) 11 9457-9857",
+        R.drawable.sample5
+    ),
+    Contact(
+        "Carol",
+        "(55) 11 9543-4587",
+        R.drawable.sample3
+    ),
+    Contact(
+        "Ana",
+        "(55) 11 9854-5487",
+        R.drawable.sample4
+    ),
+    Contact(
+        "Antonio",
+        "(55) 11 9743-9857",
+        R.drawable.sample5
+    ),
+    Contact(
+        "Suzana",
+        "(55) 11 9854-9857",
+        R.drawable.sample6
+    ),
+    Contact(
+        "Sofia",
+        "(55) 11 9421-9857",
+        R.drawable.sample7
+    ),
+    Contact(
+        "Iuri",
+        "(55) 11 9657-4433",
+        R.drawable.sample8
+    ),
+    Contact(
+        "Felipe",
+        "(55) 11 9854-9857",
+        R.drawable.sample9
+    ),
+    Contact(
+        "Augusto",
+        "(55) 11 9643-9857",
+        R.drawable.sample10
+    ),
+    Contact(
+        "Beatriz",
+        "(55) 11 9324-5478",
+        R.drawable.sample11
+    ),
+    Contact(
+        "Augusto",
+        "(55) 11 9258-9857",
+        R.drawable.sample12
+    ),
+    Contact(
+        "Fernando",
+        "(55) 11 9147-1234",
+        R.drawable.sample13
+    ),
+)
